6164879c3f8ad4dfd9bff478f33c46cbc841c90e
Jacob Carpenter <github@jacobcarpenter.com>
2020-10-22 18:26:37 -0700

    add index page

--- pages/_app.js
+++ pages/_app.js
@@ -43,31 +43,58 @@ export default function App({ Component, pageProps }) {
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
 			<Box fontSize={3}>
-				<HorizontalStack
-					as="header"
+				<Box
+					display="flex"
 					paddingX={4}
 					paddingY={3}
-					spacing={3}
 					bg="pageHeaderBackground"
 					color="pageHeaderColor"
-					justifyContent="flex-end"
-					css={`
-						white-space: nowrap;
-					`}
 				>
-					<Box color="pageHeaderSecondaryColor">top</Box>
-					<HeaderLink href="/all-time">all-time</HeaderLink>
-					<Box
-						width="2px"
-						alignSelf="stretch"
-						bg="pageHeaderSecondaryColor"
-					/>
-					<HeaderLink href="/latest">latest</HeaderLink>
-				</HorizontalStack>
-				<Box paddingX={4}>
+					<Box>
+						<HeaderLink href="/">
+							<HomeIcon />
+							&emsp;games gallery
+						</HeaderLink>
+					</Box>
+					<HorizontalStack
+						as="header"
+						flex="1 1"
+						spacing={3}
+						justifyContent="flex-end"
+						css={`
+							white-space: nowrap;
+						`}
+					>
+						<HeaderLink href="/latest">latest</HeaderLink>
+						<Box
+							width="2px"
+							alignSelf="stretch"
+							bg="pageHeaderSecondaryColor"
+						/>
+						<HeaderLink href="/all-time">all-time</HeaderLink>
+					</HorizontalStack>
+				</Box>
+				<Box paddingX={3}>
 					<Component {...pageProps} />
 				</Box>
 			</Box>
 		</ThemeProvider>
+	);
+}
+
+function HomeIcon() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 50 50"
+			fill="currentcolor"
+			height="18px"
+			css={`
+				position: relative;
+				top: 3px;
+			`}
+		>
+			<path d="M 25 1.0507812 C 24.7825 1.0507812 24.565859 1.1197656 24.380859 1.2597656 L 1.3808594 19.210938 C 0.95085938 19.550938 0.8709375 20.179141 1.2109375 20.619141 C 1.5509375 21.049141 2.1791406 21.129062 2.6191406 20.789062 L 4 19.710938 L 4 46 C 4 46.55 4.45 47 5 47 L 19 47 L 19 29 L 31 29 L 31 47 L 45 47 C 45.55 47 46 46.55 46 46 L 46 19.710938 L 47.380859 20.789062 C 47.570859 20.929063 47.78 21 48 21 C 48.3 21 48.589063 20.869141 48.789062 20.619141 C 49.129063 20.179141 49.049141 19.550938 48.619141 19.210938 L 25.619141 1.2597656 C 25.434141 1.1197656 25.2175 1.0507812 25 1.0507812 z M 35 5 L 35 6.0507812 L 41 10.730469 L 41 5 L 35 5 z"></path>
+		</svg>
 	);
 }
---
+++ pages/index.js
@@ -0,0 +1,83 @@
+import Link from 'next/link';
+import { Box } from '../components/primitives';
+
+export default function Home() {
+	return (
+		<Box padding={3} paddingBottom={6} maxWidth="800px">
+			<p>
+				Hi!{' '}
+				<span role="img" aria-label="Waving hand">
+					👋
+				</span>{' '}
+				Welcome to an unofficial gallery view for the{' '}
+				<a href="https://forums.makecode.com/">MakeCode forums</a>!
+			</p>
+			<p>
+				<strong>
+					<a href="https://arcade.makecode.com">MakeCode Arcade</a> is
+					great
+				</strong>
+				, and I love seeing the things people build in it! Sometimes
+				it’s hard for me to keep track of the new games that are showing
+				up, though.{' '}
+				<span role="img" aria-label="Smiling guy with a sweat drop">
+					😅
+				</span>
+			</p>
+			<p>
+				This site is an attempt to reformat the forums into a more
+				browsable game library. All of the pages of this site have
+				direct equivalents on the forums:
+			</p>
+			<dl>
+				<PageHeading>
+					<Link href="/latest">
+						<a>Latest</a>
+					</Link>
+				</PageHeading>
+				<dd>
+					a view of the{' '}
+					<a href="https://forum.makecode.com/tag/game?ascending=false&amp;order=created">
+						most recently created topics that feature the game tag
+					</a>
+					.
+				</dd>
+
+				<PageHeading>
+					<Link href="/all-time">
+						<a>All-time</a>
+					</Link>
+				</PageHeading>
+				<dd>
+					a view of the{' '}
+					<a href="https://forum.makecode.com/top/all?tags[]=game&amp;order=op_likes">
+						all time top topics ordered by likes on the first post
+					</a>
+					.
+				</dd>
+
+				<PageHeading>
+					<Link href="/author/527">
+						<a>Author</a>
+					</Link>
+				</PageHeading>
+				<dd>
+					a view of the{' '}
+					<a href="https://forum.makecode.com/search?q=tag%3Agame%20%40jacob_c%20in%3Afirst%20order%3Alikes">
+						games posted by a specific author
+					</a>
+					; this one you get to by clicking on the link in a game
+					tile.
+				</dd>
+			</dl>
+		</Box>
+	);
+}
+
+function PageHeading({ children }) {
+	return (
+		<Box as="dt" mt={3} fontWeight="bold">
+			{children}
+		</Box>
+	);
+}
